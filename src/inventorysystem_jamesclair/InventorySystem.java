c3b97828b7603d6e416a3869d0ad573688759ca5
james.clair <james.clair@logrhythm.com>
2019-08-18 14:20:30 -0600

    added skeleton controllers, button handles and field fxids to views

--- src/inventorysystem_jamesclair/InventorySystem.java
+++ src/inventorysystem_jamesclair/InventorySystem.java
@@ -19,7 +19,7 @@ public class InventorySystem extends Application {
 	
 	@Override
 	public void start(Stage stage) throws Exception {
-		Parent root = FXMLLoader.load(getClass().getResource("FXMLDocument.fxml"));
+		Parent root = FXMLLoader.load(getClass().getResource("MainMenu.fxml"));
 		
 		Scene scene = new Scene(root);
 		
--- src/view_controller/MainMenuController.java
+++ src/view_controller/MainMenuController.java
@@ -7,7 +7,13 @@ package view_controller;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
 
 /**
  * FXML Controller class
@@ -16,6 +22,31 @@ import javafx.fxml.Initializable;
  */
 public class MainMenuController implements Initializable {
 
+	@FXML
+	private TextField searchPartTxt;
+	@FXML
+	private TableView<?> partsTableView;
+	@FXML
+	private TableColumn<?, ?> partIdCol;
+	@FXML
+	private TableColumn<?, ?> partNameCol;
+	@FXML
+	private TableColumn<?, ?> partInventoryLvlCol;
+	@FXML
+	private TableColumn<?, ?> partPriceCol;
+	@FXML
+	private TextField searchProductTxt;
+	@FXML
+	private TableView<?> productsTableView;
+	@FXML
+	private TableColumn<?, ?> productIdCol;
+	@FXML
+	private TableColumn<?, ?> productNameCol;
+	@FXML
+	private TableColumn<?, ?> productInventoryLvlCol;
+	@FXML
+	private TableColumn<?, ?> productPriceCol;
+
 	/**
 	 * Initializes the controller class.
 	 */
@@ -23,5 +54,42 @@ public class MainMenuController implements Initializable {
 	public void initialize(URL url, ResourceBundle rb) {
 		// TODO
 	}	
+
+	@FXML
+	private void onActionSearchPart(ActionEvent event) {
+	}
+
+	@FXML
+	private void onActionAddPart(ActionEvent event) {
+	}
+
+	@FXML
+	private void onActionModifyPart(ActionEvent event) {
+	}
+
+	@FXML
+	private void onActionDeletePart(ActionEvent event) {
+	}
+
+	@FXML
+	private void onActionSearchProduct(ActionEvent event) {
+	}
+
+	@FXML
+	private void onActionAddProduct(ActionEvent event) {
+	}
+
+	@FXML
+	private void onActionModifyProduct(ActionEvent event) {
+	}
+
+	@FXML
+	private void onActionDeleteProduct(ActionEvent event) {
+	}
+
+	@FXML
+	private void onActionExit(ActionEvent event) {
+	}
+
 	
 }
